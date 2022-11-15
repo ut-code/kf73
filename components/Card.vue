79359127eb68bbf95b033f66f1abd30098726aff
taku <integral0523@gmail.com>
2022-11-15 22:25:55 +0900

    カードコンポーネントの作成

---
+++ components/Card.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card class="my-7">
+    <slot />
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'CardComponent',
+})
+</script>
