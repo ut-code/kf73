0beac38508b77c1d4d6e37a4a2f5f8eed426e60e
taku <integral0523@gmail.com>
2022-11-15 22:26:37 +0900

    コンテンツ情報コンポーネントの作成（途中）

---
+++ components/Top/Kikaku/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <card>
+    <v-card-title class="headline primary white--text mb-3">
+      {{ number }}. {{ content.title }}
+    </v-card-title>
+    <v-card-text>
+      <p>対象：{{ content.target }}</p>
+      <p v-for="message in content.messages" :key="message">{{ message }}</p>
+      <external-button :url="content.url">
+        このコンテンツで遊ぶ
+      </external-button>
+    </v-card-text>
+  </card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Card from '~/components/Card.vue'
+import ExternalButton from '@/components/Button/External.vue'
+
+export default Vue.extend({
+  name: 'KikakuCardComponent',
+  components: { Card, ExternalButton },
+  props: {
+    number: { type: Number, required: true },
+    content: { type: Object, required: true },
+  },
+})
+</script>
