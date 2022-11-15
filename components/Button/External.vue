8bf33ba700b1cd4698199e2279839e14ae5324fc
taku <integral0523@gmail.com>
2022-11-15 22:25:23 +0900

    ボタンコンポーネントの作成

---
+++ components/Button/External.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-btn block :href="url" target="_blank" class="my-4">
+    <slot />
+  </v-btn>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'ExternalButtonComponent',
+  props: {
+    url: { type: String, required: true },
+  },
+})
+</script>
