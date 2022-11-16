79b936cb3779eee7e48a6d8a271dcf212dabb080
taku <integral0523@gmail.com>
2022-11-16 21:09:00 +0900

    OGP用にurlを更新

--- assets/baseInfo.js
+++ assets/baseInfo.js
@@ -2,4 +2,4 @@ export const shortName = 'あなたのためのプログラミング'
 export const title = `ut.code(); 第73回駒場祭特設ウェブサイト「${shortName}」`
 export const description =
   'どんどん身近なものとなるプログラミング。誰もがより興味を持てるような企画です。子供も大人も、未経験者も経験者もぜひ！'
-export const url = 'kf73.utcode.net'
+export const url = 'https://kf73.utcode.net'
