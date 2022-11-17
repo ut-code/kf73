54f2a62d4ad1bebeb8c4ae59a7a968adae388a7b
taku <integral0523@gmail.com>
2022-11-17 18:42:13 +0900

    external buttonにイベント設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,7 @@ export default {
   css: ['~/assets/style.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['plugins/types.ts'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
