fe6fae8c3f2753853a4e01914a2d2f770422d2fc
taku <integral0523@gmail.com>
2022-11-17 22:30:35 +0900

    コンテンツひとまず完成

--- assets/contents.ts
+++ assets/contents.ts
@@ -12,34 +12,59 @@ export const contents: Content[] = [
     url: 'https://festival.utcode.net',
   },
   {
-    title: 'ゲームのAIを組んでみよう',
+    title: 'Code vs Code',
     target: '小学生〜大人',
-    messages: [],
+    messages: [
+      'ブロックをならべて、つなげて、自分だけのパイロットAIをプログラムしよう！',
+      '作ったプログラムで、企画に参加してくださった他の皆さんと対戦できます。',
+      '最強AIの座を手にするのは一体誰だ！？',
+      'オンラインでの公開は11月19日以降を予定しています。',
+    ],
     url: '',
   },
   {
-    title: '機械学習に挑戦！？',
-    target: '小学3年生〜大人',
-    messages: [],
-    url: '',
+    title: '強化学習AIと『ニュートリーコ』で対戦！',
+    target: '小学生〜大人',
+    messages: [
+      '強化学を使って、『ニュートリーコ』のゲームAIを作ってみました。',
+      'ルールは簡単です。ぜひ挑戦してみてください！',
+    ],
+    url: 'https://neutreeko.herokuapp.com/',
   },
+  {
+    title: '強化学習AIと『6x6リバーシ』で対戦！',
+    target: '小学生〜大人',
+    messages: [
+      '強化学習を使って、『6x6リバーシ』のゲームAIも作ってみました。',
+      'こちらもルールは簡単です。ぜひ挑戦してみてください！',
+    ],
+    url: 'https://reversi-6x6.herokuapp.com/',
+  },
+
   {
     title: 'タイピングゲーム',
     target: 'キーボードが打てればOK',
-    messages: [],
+    messages: [
+      'タイピングは、プログラマーはもちろん、多くの大学生にとって必須のスキルです。',
+      '友達と遊びながら鍛えたい、そう思ったメンバーがゲームを作っちゃいました！',
+      '親子・友達同士での対戦してみませんか？　東大生に挑戦していきませんか？',
+    ],
     url: 'https://typing-game.onrender.com/',
   },
   {
     title: 'pl-workshop',
     target: '中学生〜大人 プログラミング経験者',
-    messages: [],
+    messages: [
+      'プログラミングをやったことがある人でも、プログラミング言語を使ったことがある人は少ないのではないでしょうか？',
+      'JavaScriptのライブラリ、PEG.jsを使って四則演算や変数、条件分岐、関数が使える簡単なプログラミング言語を作ってみましょう！',
+    ],
     url: 'https://pl-workshop.github.io/pl-workshop-text/',
   },
   {
-    title: 'はじめてのアプリづくり！',
-    target: '小学校1年生〜小学校6年生',
-    messages: [],
-    url: '',
+    title: 'あなたのスマホで動く！　ウェブアプリをつくろう',
+    target: '小学生〜大人',
+    messages: [''],
+    url: 'https://pwa-maker.onrender.com/',
   },
   {
     title: 'ウェブ開発入門編',
