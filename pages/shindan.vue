b00195b68df95986483038b81eaec09cc34146e6
taku <integral0523@gmail.com>
2022-11-18 21:59:04 +0900

    診断作成中

---
+++ pages/shindan.vue
@@ -0,0 +1,22 @@
+<template>
+  <card>
+    <v-card-title class="headLine">プログラマー診断</v-card-title>
+    <v-card-text>
+      <p>question</p>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn text> yes </v-btn>
+      <v-btn text> no </v-btn>
+    </v-card-actions>
+  </card>
+</template>
+
+<script>
+import Card from '~/components/Card.vue'
+
+export default {
+  name: 'IndexPage',
+  components: { Card },
+  data: () => ({}),
+}
+</script>
